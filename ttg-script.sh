3cf90efa5920c98c57edb9b757092f2ffe54c5a9
John Zygoulakis <67337564+johnzgl00@users.noreply.github.com>
2021-08-15 17:30:36 +0300

    Update ttg-script.sh

--- ttg-script.sh
+++ ttg-script.sh
@@ -1,15 +1,30 @@
 #! /bin/bash
 
+blue='\033[1;34m'
+bold='\033[1;37m'
+NC='\033[0m'
 
-echo -e "Enter file/folder to send (with location if needed):"
+echo -e "${blue} ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ ${NC}"
+echo -e "${blue}▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌${NC}"
+echo -e "${blue} ▀▀▀▀█░█▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ${NC}"
+echo -e "${blue}     ▐░▌          ▐░▌     ▐░▌          ${NC}"
+echo -e "${blue}     ▐░▌          ▐░▌     ▐░▌ ▄▄▄▄▄▄▄▄ ${NC}"
+echo -e "${blue}     ▐░▌          ▐░▌     ▐░▌▐░░░░░░░░▌${NC}"
+echo -e "${blue}     ▐░▌          ▐░▌     ▐░▌ ▀▀▀▀▀▀█░▌${NC}"
+echo -e "${blue}     ▐░▌          ▐░▌     ▐░▌       ▐░▌${NC}"
+echo -e "${blue}     ▐░▌          ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌${NC}"
+echo -e "${blue}     ▐░▌          ▐░▌     ▐░░░░░░░░░░░▌${NC}"
+echo -e "${blue}      ▀            ▀       ▀▀▀▀▀▀▀▀▀▀▀ ${NC}"
+
+echo -e "${bold}Enter file/folder to send (with location if needed):${NC}"
 read file
-echo -e "Enter Ip-Address:"
+echo -e "${bold}Enter Ip-Address:${NC}"
 read ip
-echo -e "Enter port:"
+echo -e "${bold}Enter port:${NC}"
 read port
-echo -e "Enter Username:"
+echo -e "${bold}Enter Username:${NC}"
 read username
-echo -e "Enter reciever location:"
+echo -e "${bold}Enter reciever location:${NC}"
 read location
 
 sudo scp -P "$port" "$file" "$username"@"$ip":"$location"
